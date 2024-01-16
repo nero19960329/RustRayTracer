d9ef0370710c2d598f587fd35e3264f60ff56a50
nero19960329 <wangzhao2013tsinghua@gmail.com>
2024-01-16 10:26:50 +0900

    feat: render config

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,40 @@ mod renderer;
 mod scene;
 
 use camera::PerspectiveCamera;
+use clap::Parser;
 use math::{Point, Vec3};
-use renderer::{render, IMAGE_HEIGHT, IMAGE_WIDTH};
+use renderer::{render, RenderConfig};
 use scene::Scene;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(
+    author,
+    version,
+    about,
+    long_about = "A simple raytracer written in Rust."
+)]
+struct Args {
+    #[arg(short, long)]
+    config: String,
+
+    #[arg(short, long)]
+    output: String,
+}
 
 fn main() {
+    let args = Args::parse();
+
+    let render_config: RenderConfig = toml::from_str(&fs::read_to_string(args.config).unwrap())
+        .expect("Failed to parse config file");
     let scene = Scene::new();
     let camera = PerspectiveCamera::new(
         Point::new(50.0, 52.0, 295.6),
         Point::new(50.0, 39.4, 0.0),
         Vec3::new(0.0, 1.0, 0.0),
         30.0,
-        IMAGE_WIDTH as f32 / IMAGE_HEIGHT as f32,
+        render_config.image.width as f32 / render_config.image.height as f32,
     );
-    let img = render(&scene, &camera);
-    img.save("output.png").unwrap();
+    let img = render(&render_config, &scene, &camera);
+    img.save(args.output).unwrap();
 }
