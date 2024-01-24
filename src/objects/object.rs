d9b219bc474cb2e4054019d86e014273a2b802b5
Zhao Wang <wangzhao2013tsinghua@gmail.com>
2024-01-24 21:43:05 +0900

    refactor: object config

--- src/objects/object.rs
+++ src/objects/object.rs
@@ -1,8 +1,7 @@
-use super::super::math::{unwrap_matrix4d_config_to_matrix4d, Ray};
+use super::super::math::Ray;
 use super::common::HitRecord;
 use super::plane::{Plane, PlaneConfig};
 use super::sphere::{Sphere, SphereConfig};
-use cgmath::InnerSpace;
 use serde::Deserialize;
 
 pub enum Object {
@@ -20,26 +19,8 @@ pub enum ObjectConfig {
 impl ObjectConfig {
     pub fn to_object(&self) -> Object {
         match self {
-            ObjectConfig::Sphere(config) => Object::Sphere(
-                Sphere {
-                    center: config.center.to_point(),
-                    radius: config.radius,
-                    material: config.material.to_material(),
-                }
-                .transform(&unwrap_matrix4d_config_to_matrix4d(
-                    config.transform.as_ref(),
-                )),
-            ),
-            ObjectConfig::Plane(config) => Object::Plane(
-                Plane {
-                    point: config.point.to_point(),
-                    normal: config.normal.to_vec3().normalize(),
-                    material: config.material.to_material(),
-                }
-                .transform(&unwrap_matrix4d_config_to_matrix4d(
-                    config.transform.as_ref(),
-                )),
-            ),
+            ObjectConfig::Sphere(config) => Object::Sphere(config.to_instance()),
+            ObjectConfig::Plane(config) => Object::Plane(config.to_instance()),
         }
     }
 }
